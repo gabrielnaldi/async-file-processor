025fa0f18ab3296e01b2bdd16f0f36c3533f51dd
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-01-19 10:44:55 -0300

    test: ensure it can not mark a COMPLETED log as COMPLETED

--- src/__tests__/domain/entities/log.entity.spec.ts
+++ src/__tests__/domain/entities/log.entity.spec.ts
@@ -175,4 +175,23 @@ describe('Log - Entity', () => {
       expect((error as LogError).code).toBe('COMPLETE_A_PENDING');
     }
   });
+
+  it('should not allow a COMPLETED Log to me marked as COMPLETED', () => {
+    const log_entity = Log.create({
+      id: '1',
+      filePath: '/example/path',
+    });
+
+    log_entity.markAsProcessing();
+
+    const fn = () => log_entity.markAsCompleted();
+
+    try {
+      fn();
+    } catch (error) {
+      expect(error).toBeInstanceOf(LogError);
+      expect((error as LogError).name).toBe('LogError');
+      expect((error as LogError).code).toBe('ALREADY_COMPLETED');
+    }
+  });
 });
