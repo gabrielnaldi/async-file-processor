2a6c03d411b3b7335489fb1a36c4069ec4b507be
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-01-25 14:29:02 -0300

    refactor: adjust return when no log are found

--- src/domain/repositories/log.repository.ts
+++ src/domain/repositories/log.repository.ts
@@ -2,6 +2,6 @@ import { Log } from '../entities/log.entity';
 
 export abstract class LogRepository {
   abstract save(log: Log): Promise<Log>;
-  abstract findById(id: string): Promise<Log>;
+  abstract findById(id: string): Promise<Log | null>;
   abstract listAll(): Promise<Log[]>;
 }
